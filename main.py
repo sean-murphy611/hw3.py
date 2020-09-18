41e9ecf4bbbba3b1b7c81dafd9d5d341ffefc95d
seanmurphy611 <spm6407@psu.edu>
2020-09-18 22:18:31 +0000

    digit_sum

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 num = int(input("Enter an int: "))
-result = 0
+digit_sum = 0
 hold = num
 
 
 while num > 0:
     rem = num % 10
-    result = result + rem
+    digit_sum = digit_sum + rem
     num = int(num/10)
 
 
-print("sum of digit", hold, "is: ", result)
+print("sum of digit", hold, "is: ", digit_sum)
